bbd1b7048a29700fd149af6cebc32bff9ebd1901
ubAI6689 <ub.ai6689@gmail.com>
2022-08-20 00:30:29 +0700

    aesthetic

--- 800/src/A_Boring_Apartments.cpp
+++ 800/src/A_Boring_Apartments.cpp
@@ -9,10 +9,10 @@ int main()
     while(t--)
     {
         int x; cin >> x;
-        if (x > 1000) cout << ((x/1111) - 1)*10 + 10 << endl;
-        else if (x > 100) cout << ((x/111) - 1)*10 + 6 << endl;
-        else if (x > 10) cout << ((x/11) - 1)*10 + 3 << endl;
-        else if (x < 10) cout << (x - 1)*10 + 1 << endl;
+             if (x > 1000) cout << ((x - 1)/1111)*10 + 10 << endl;
+        else if (x > 100)  cout <<  ((x - 1)/111)*10 + 6  << endl;
+        else if (x > 10)   cout <<   ((x - 1)/11)*10 + 3  << endl;
+        else if (x < 10)   cout <<    ((x - 1)/1)*10 + 1  << endl;
     }
     return 0;
 }
