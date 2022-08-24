219971c5f86d3f9bde1b07b0b1fa53a72580449e
ubAI6689 <ub.ai6689@gmail.com>
2022-08-24 17:18:43 +0700

    900 : 7

---
+++ 900/src/B_Tiling_Challenge.cpp
@@ -0,0 +1,38 @@
+// https://codeforces.com/problemset/problem/1150/B
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    char tiles[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> tiles[i][j];
+        }
+    }
+
+    char hash = '#';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (tiles[i][j] == '.' )
+            {
+                if (j == 0 || j == n - 1) {cout << "NO\n"; return 0;}
+                else if (tiles[i+1][j-1] == '.' && tiles[i+1][j] == '.' && tiles[i+1][j+1] == '.' && tiles[i+2][j] == '.')
+                {   
+                    tiles[i][j] = hash;
+                    tiles[i+1][j-1] = hash; tiles[i+1][j] = hash; tiles[i+1][j+1] = hash;
+                    tiles[i+2][j] = hash;
+                    continue;
+                }
+                else {cout << "NO\n"; return 0;}
+            }
+        }
+    }
+    cout << "YES\n";
+    return 0;
+}
