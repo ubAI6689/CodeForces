4d668d7df120ecc4fbf7711918e0c1de8ade4093
ubAI6689 <ub.ai6689@gmail.com>
2022-08-28 12:13:56 +0700

    Deadly laser WIP. Wrong at test 2 no 99th

--- Div2/20220827/B_Deadly_Laser.cpp
+++ Div2/20220827/B_Deadly_Laser.cpp
@@ -1,6 +1,41 @@
 // https://codeforces.com/contest/1721/problem/B
 #include <iostream>
 using namespace std;
+
+bool x_clear(int n, int m, int s_x, int s_y, int d)
+{
+    // navigate x =1,2,3...,n at y == 1 && y == m
+    int count=0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (abs(i-s_x) <= d && abs(1-s_y) <= d) {count++;break;}
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (abs(i-s_x) <= d && abs(m-s_y) <= d) {count++;break;}
+    }
+    if (count == 2) return false;
+    else return true;
+}
+
+bool y_clear(int n, int m, int s_x, int s_y, int d)
+{
+    // navigate y = 1,2,3,...,m at x == 1 && x = n
+    int count=0;
+    for (int i = 1; i <= m; i++)
+    {
+        if (abs(1-s_x) <= d && abs(i-s_y) <= d) {count++;break;}
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        if (abs(n-s_x) <= d && abs(i-s_y) <= d) {count++;break;}
+    }
+    if (count == 2) return false;
+    else return true;
+}
+
 int main()
 {
     int t; cin >> t;
@@ -8,22 +43,11 @@ int main()
     {
         int n,m,s_x,s_y,d;
         cin>>n>>m>>s_x>>s_y>>d;
-        bool x_clear=false;
-        bool y_clear=false;
 
-        for (int i = 1; i <= n; i++)
-        {
-            if (abs(i-s_x)<=d && abs(1-s_y)<=d){break;}
-            x_clear=true;
-        }
+        bool x_test = x_clear(n,m,s_x,s_y,d);
+        bool y_test = y_clear(n,m,s_x,s_y,d);
 
-        for (int i = 1; i <= m; i++)
-        {
-            if (abs(1-s_x)<=d && abs(i-s_y)<=d){break;}
-            y_clear=true;
-        }
-
-        if(x_clear&&y_clear)
+        if(x_test && y_test)
         {
             int step = m-1+n-1;
             cout<<step<<endl;
