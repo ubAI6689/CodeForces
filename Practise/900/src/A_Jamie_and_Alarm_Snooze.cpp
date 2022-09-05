fad6aa9dbec3d99931d6c78f8dfd143fc7952276
ubAI6689 <ub.ai6689@gmail.com>
2022-09-05 15:50:53 +0700

    900 : 34

---
+++ Practise/900/src/A_Jamie_and_Alarm_Snooze.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+bool isLucky(int hh, int mm)
+{
+    if (hh%10 == 7 || (hh/10)%10 == 7 || mm%10==7 || (mm/10)%10==7)
+        return true;
+    else return false;
+}
+
+int main()
+{
+    int n; std::cin>>n;
+    int hh, mm;
+    std::cin>>hh>>mm;
+    int count = 0;
+    
+    while(!isLucky(hh, mm))
+    {
+        if (mm - n < 0)
+        {
+            if (hh - 1 < 0)
+            {
+                hh += 24;
+            }
+            hh -= 1;
+            mm += 60;
+        }
+        mm -= n;
+        count++;
+    }
+    std::cout<<count;
+    return 0;
+}
