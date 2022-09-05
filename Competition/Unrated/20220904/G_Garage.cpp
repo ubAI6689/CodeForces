ba84f416f94e7b6e67034f0496d3347484dc5d26
ubAI6689 <ub.ai6689@gmail.com>
2022-09-05 08:28:21 +0700

    Contest 20220904

---
+++ Competition/Unrated/20220904/G_Garage.cpp
@@ -0,0 +1,15 @@
+// C++ program to generate pythagorean
+// triplets smaller than a given limit
+#include <bits/stdc++.h>
+typedef long long ll;
+ 
+// Driver Code
+int main()
+{
+    ll N; std::cin>>N;
+
+    
+
+    
+    return 0;
+}
