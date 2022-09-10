89e14576a1bac7388645974becbfb77da68824b3
ubAI6689 <ub.ai6689@gmail.com>
2022-09-10 23:25:09 +0700

    1000 : 12

--- Practise/1000/B_Nastya_Is_Playing_Computer_Games.cpp
+++ Practise/1000/B_Nastya_Is_Playing_Computer_Games.cpp
@@ -6,14 +6,9 @@ int main()
     std::cin>>n>>k;
 
     int cnt=0;
-    cnt+=2*n+1;   // throw stone + pick coin for every manhole, 
-                  // +1 because one of the manhole have two stones on it
-    
-    // move towards nearest end first
-    if (k<=n/2) 
-        cnt+=n-1+k-1; // total step taken k --> 1 --> n    
-    else
-        cnt+=n-k+n-1; // total step from k-->n-->1
+    cnt+=2*n+1; // throw stone + pick coin for every manhole, +1 because one of the manhole have two stones on it
+    cnt+=std::min(k-1, n-k); // move towards nearest end first
+    cnt+=n-1; // end to end
     
     std::cout<<cnt;
     return 0;
