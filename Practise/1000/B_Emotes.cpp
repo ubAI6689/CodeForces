b8ad7e9740bba9f6271bfbdd5723793eea1dde7e
ubAI6689 <ub.ai6689@gmail.com>
2022-09-14 21:47:31 +0700

    1000 : 14

--- Practise/1000/B_Emotes.cpp
+++ Practise/1000/B_Emotes.cpp
@@ -8,9 +8,7 @@ int main()
     std::vector<ll> emote;
     for (int i=0,x; i<n&&std::cin>>x; i++) emote.push_back(x);
     sort(emote.begin(), emote.end(), std::greater<ll>());
-    // for (auto it: emote) std::cout<<it<<" ";
     
     std::cout<<m/(k+1)*1ll*(emote[0]*1ll*k+emote[1])+m%(k+1)*1ll*emote[0]<<std::endl;
-    // std::cout<<sum;
     return 0;
 }
