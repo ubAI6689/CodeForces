01f1e1fcee002e49ef48639984251d915d3f756e
ubAI6689 <ub.ai6689@gmail.com>
2022-10-18 10:11:00 +0700

    Edu137: correction

--- Competition/Div2/Edu137/C_Save_the_Magazines.cpp
+++ Competition/Div2/Edu137/C_Save_the_Magazines.cpp
@@ -8,43 +8,34 @@ int main()
     {
         int n; std::cin>>n;
         std::string lid; std::cin>>lid;
-        int original_sum=0;
         int array[n];
         for (int i=0; i<n; i++)
         {
             std::cin>>array[i];
-            if (lid[i]=='1')
-                original_sum+=array[i];
         }
 
-        int res=0;
-        for (int i=1; i<lid.length();i++)
+        for (int i=0, j=-1; i<n; i++)
         {
-            if ((lid[i]=='1' && lid[i-1]=='0') && (array[i]<=array[i-1]))
+            if (lid[i]=='0')
             {
-                lid[i]='0';
-                res+=array[i-1];
-                continue;
+                j=i;
             }
-            if ((lid[i]=='1' && lid[i-1]=='0') && (array[i]>array[i-1]))
+            else if (j>=0 && array[i]<array[j])
             {
-                if (i==lid.length()-1)
-                {
-                    res+=array[i];
-                    continue;
-                }
-                if (lid[i+1]=='1' && array[i+1]<=array[i-1])
-                {
-                    lid[i]='0';
-                    res+=array[i-1];
-                    continue;
-                }
+                std::swap(lid[i],lid[j]);
+                j=i;
             }
-            if (lid[i]=='1')
-                res+=array[i];
         }
-        // std::cout<<lid<<'\n';
-        std::cout<<res<<'\n';
+
+        int ans = 0;
+        for (int i=0; i<n; i++)
+        {
+            if (lid[i]=='1')
+            {
+                ans+=array[i];
+            }
+        }
+        std::cout<<ans<<'\n';
     }
     return 0;
 }
