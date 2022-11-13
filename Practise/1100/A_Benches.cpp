d256d020b8ebece5b19921ca231e928ea2121fcb
ubAI6689 <ub.ai6689@gmail.com>
2022-11-13 16:55:26 +0700

    1100 : 19

--- Practise/1100/A_Benches.cpp
+++ Practise/1100/A_Benches.cpp
@@ -12,39 +12,20 @@ int main()
         // number of people sitting on each bench initially
         std::cin>>a[i];
     }
-
-    // copy the array a to b
-    int b[n];
-    for (int i = 0; i < n; i++)
-    {
-        b[i] = a[i];
-    }
-
     // distribute m people among the benches 1 by 1 to get minimum maximum
     // sort the array a in ascending order
     std::sort(a, a+n);
+    int max = a[n-1]+m ; // maximum number of people sitting on a bench
 
-    // distribute m people among the benches 1 by 1
+    // distribute m people among the benches 1 by 1 to get smallest maximum
     for (int i = 0; i < m; i++)
     {
         a[0]++; // add 1 to the first bench
         std::sort(a, a+n); // sort the array a in ascending order
     }
-
     // the maximum number of people sitting on a bench is the last element of the array a
     int min = a[n-1];
 
-    // sort the array b in descending order
-    std::sort(b, b+n, std::greater<int>());
-    
-    // distribute m people among the benches 1 by 1 to get maximum maximum
-    for (int i = 0; i < m; i++)
-    {
-        b[0]++; // add 1 to the first bench
-        std::sort(b, b+n, std::greater<int>()); // sort the array b in descending order
-    }
-    int max = b[0];
-    
     std::cout<<min<<" "<<max<<std::endl;
     return 0;
 }
