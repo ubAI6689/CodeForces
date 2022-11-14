8a95def19802c0549fbf5d05b6ef377ff347650f
ubAI6689 <ub.ai6689@gmail.com>
2022-11-14 10:43:05 +0700

    1000 : 24

--- Practise/1100/B_Substrings_Sort.cpp
+++ Practise/1100/B_Substrings_Sort.cpp
@@ -12,11 +12,15 @@ int main()
     // sort strings by length
     sort(s.begin(),s.end(),[](string a,string b){return a.length()<b.length();});
     // check if strings[i] is a substring of strings[i+1]
-    for(int i=0;i<n-1;i++) if(s[i+1].find(s[i])==string::npos) 
-    {
-        cout<<"NO"<<endl; 
-        return 0;
+    for(int i=0;i<n-1;i++) 
+    {   
+        if(s[i+1].find(s[i])==string::npos) 
+        {
+            cout<<"NO"<<endl; 
+            return 0;
+        }
     }
+    
     cout<<"YES"<<endl;
     for(int i=0;i<n;i++) 
         cout<<s[i]<<endl;
