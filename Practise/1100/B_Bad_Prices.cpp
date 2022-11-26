aea9a02061f55bd94bbdb28a43a7a9bfc832f6fa
ubAI6689 <ub.ai6689@gmail.com>
2022-11-26 20:13:18 +0700

    1100 : 39

--- Practise/1100/B_Bad_Prices.cpp
+++ Practise/1100/B_Bad_Prices.cpp
@@ -1,42 +1,26 @@
 #include <bits/stdc++.h>
 int main()
 {
-    int T; std::cin>>T;
+    int T; std::cin >> T;
     while(T--)
     {
-        int n; std::cin>>n;
+        int n; std::cin >> n;
         int a[n];
-        for(int i=0;i<n;i++)
+        for(int i = 0; i < n; i++)
         {
-            std::cin>>a[i];
+            std::cin >> a[i];
         }
         
-        int count=0;
-        int bad=0;
-        int min=a[0];
-        int min_index=0;
-        for(int i=0;i<n-1;i++)
-        {     
-            if (a[i]<a[i+1])
+        int ans = 0;
+        int min = INT_MAX;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(a[i] > min) // if current element is greater than the current minimum, then increase answer
             {
-                count++;
+                ans++;
             }
-
-            if (a[i]>a[i+1])
-            {
-                if (a[i+1]<=min)
-                {
-                    bad=0;
-                    min_index=i+1;
-                    min=a[min_index];
-                }
-                else if (a[i+1]>min)
-                {
-                    bad+=count;
-                    count=0;
-                }
-            }
+            min = std::min(min, a[i]); // update the minimum to the minimum of the current minimum and the current element
         }
-        std::cout<<bad+min_index<<'\n';  
+        std::cout << ans << std::endl;
     }   
 }
